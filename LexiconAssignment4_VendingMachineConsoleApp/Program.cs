5e65a4d435fa9c29b30e303ed058fbc408525106
Muzdalifa <a.muzdalifa27@gmail.com>
2021-07-08 11:10:47 +0200

    Add project files.

---
+++ LexiconAssignment4_VendingMachineConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LexiconAssignment4_VendingMachineConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
